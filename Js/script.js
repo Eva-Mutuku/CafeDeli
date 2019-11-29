8cb3c9684f4d9424db429976892ca1b9354a79c3
Eva Mutuku <mutileeva@gmail.com>
2019-11-29 01:25:58 -0800

    Create index.html file.

---
+++ Js/script.js
@@ -0,0 +1,32 @@
+var choice1 = (prompt("What food would you like to have? " + "1.Fries " +  "2.Burger " + "3.Pizza " + "4.Chicken wrap "));
+var a = "Fries";
+var b = "Burger";
+var c = "Pizza";
+var d = "Chicken wrap";
+
+if(choice1==a ||choice1==b || choice1==a ||choice1==b){
+alert("Here is your "+choice1+".");
+}
+else {alert("Sorry, we do not have that in our menu.")} 
+
+var choice2 = prompt("Do you want anything else? "+ ".Yes " +  ".No");
+var x = "Yes";
+var y = "No";
+if(choice2==Yes){
+var choice3 = prompt("Please choose what else to take." + ".Water " +  ".Soda " + ".Juice " + ".Wine");
+  
+	var e = "Water";
+	var f = "Soda";
+	var g = "Juice";
+	var h = "Wine";
+  
+	if(choice3==e ||choice3==f || choice3==g ||choice3==h){
+		alert("Here is your "+ choice3+ "Enjoy your meal and thank you for choosing us!");
+}
+		else{alert("Sorry, we do not habe that in our menu.")}
+}
+		else {alert("Enjoy your meal and thank you for choosing us!")}
+
+
+
+
